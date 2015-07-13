b508b38baeb705c58eb362f97700cb264a76a6df
GlebGeracimenko <gleb.geracimenko@gmail.com>
2015-07-13 16:50:11 +0300

    123

--- src/main/java/parser/Test.java
+++ src/main/java/parser/Test.java
@@ -1,10 +1,12 @@
 package parser;
 
+import java.net.URL;
+
 /**
  * Created by gleb on 13.07.15.
  */
 public class Test {
     public static void main(String[] args) {
-        URLParser.getLinks("http://www.coast-stores.com/all-dresses/dept/fcp-category/list");
+        URLParser.pageParsing(URLParser.getLinks("http://www.coast-stores.com/all-dresses/dept/fcp-category/list"));
     }
 }
--- src/main/java/parser/URLParser.java
+++ src/main/java/parser/URLParser.java
@@ -2,9 +2,11 @@ package parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,14 +15,36 @@ import java.util.List;
 public class URLParser {
 
     public static List<String> getLinks(String url) {
+        List<String> links = null;
         try {
             Document document = Jsoup.connect(url).get();
-            Elements elements = document.select(".title");
+            Elements elements = document.select("a[class = title]");
+            links = new ArrayList<String>();
+            for (Element element : elements) {
+                links.add(element.attr("abs:href"));
+            }
             System.out.println();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return links;
+    }
+
+    public static void pageParsing(List<String> links) {
+        for (String link : links) {
+            try {
+                System.out.println(link);
+                Document document = Jsoup.connect(link).get();
+                Element title = document.select("h1").first();
+                Element price = document.select(".product-price").first();
+                Element description = document.select(".info").first();
+                Elements allStockSize = document.select(".in-stock, .low-stock");
+                Elements images = document.select("div.product_altImages");
+                System.out.println(images.size());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
